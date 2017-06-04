fcd2bdd4149beee580e8871aa26e6a1a001dddef
“andrew <andrewwoods88@gmail.com>
2017-06-04 15:50:19 +0000

    set up devise, added bootstrap application styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  #allows user to view the homepage if they arent signed in | L195 5:00 - connected to welcome controlelr
+  before_action :authenticate_user!
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
+    #allows user to view the homepage if they arent signed in | L195 5:00 - connected to application controller
+    skip_before_action :authenticate_user!, only: [:index]
+  
   def index
   end
 end
